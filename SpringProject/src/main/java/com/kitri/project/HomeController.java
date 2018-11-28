51b7e184196e20371635fc4c6e17cc980f96f406
Gyuil Kyung <rudrbdlf1123@naver.com>
2018-11-28 12:27:59 +0900

    test4

--- SpringProject/src/main/java/com/kitri/project/HomeController.java
+++ SpringProject/src/main/java/com/kitri/project/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is test22 {}.", locale);
+		logger.info("Welcome home! The client locale is test2233 {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
