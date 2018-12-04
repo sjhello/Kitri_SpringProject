b78091f2ee59fdd6f8c8c4145359de6f7bf7a2ff
Gyuil Kyung <rudrbdlf1123@naver.com>
2018-12-04 17:25:23 +0900

    zcvd

--- SpringProject/src/main/java/com/kitri/project/notice/web/NoticeController.java
+++ SpringProject/src/main/java/com/kitri/project/notice/web/NoticeController.java
@@ -9,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.kitri.project.notice.service.NoticeService;
-
 @Controller
 public class NoticeController {
 	
