8f936ba799fc6300418eb91afa7419b95e7b8601
Gyuil Kyung <rudrbdlf1123@naver.com>
2018-12-10 15:00:12 +0900

    1210-3

--- SpringProject/src/main/webapp/resources/js/events-data.js
+++ SpringProject/src/main/webapp/resources/js/events-data.js
@@ -1,3 +1,3 @@
 var canvasEvents = {
-	'12-14-2018' : '<img src="images/ox/true.jpeg"/> <img src="images/ox/false.jpg"/>',
+		
 };
--- SpringProject/src/main/webapp/resources/js/jquery.calendario.js
+++ SpringProject/src/main/webapp/resources/js/jquery.calendario.js
@@ -34,7 +34,7 @@
 			...
 		}
 		*/
-		weeks : [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ],
+		weeks : [ 'Sunday', '월요일', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ],
 		weekabbrs : [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ],
 		months : [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
 		monthabbrs : [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
